952a156d5e47ee7c6d9c98cab0a1d99f56557068
ryutaro-asada <deneimonmaru@gmail.com>
2023-12-02 19:32:43 +0900

    7777777777

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_100043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
